9f509ced3f7c0fe97ce369a6be5a5776c6793efb
pjbooth <paul_booth_uk@hotmail.com>
2015-12-13 22:38:22 +0000

    tweak the timing loop

--- SD11Main.py
+++ SD11Main.py
@@ -31,7 +31,7 @@ error_limit = 20
 movement_count = 0
 light_count = 0
 reading_count = 0 
-loop_limit = 5						# number of readings (the reporting interval)
+loop_limit = 40						# number of readings (the reporting interval)
 
 
 
